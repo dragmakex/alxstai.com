cc01f002ba63be13775f14a3fd25dca1cf4255b0
Sticky <dragmakex@gmail.com>
2024-03-11 22:23:31 +0100

    added about me page

--- src/dropdown.tsx
+++ src/dropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import droplist from "./droplist.json"
+import Link from "next/link";
 
 function Dropdown() {
 
@@ -12,9 +13,11 @@ function Dropdown() {
                 {isOpen && (   
                     <div className="md:w-[120px] absolute flex flex-col top-10 items-center rounded-lg p-2"> 
                         {droplist.map((item, i) => (
-                                <div className="font-tisa items-center flex w-full justify-center p-1 hover:bg-gradient-to-r from-blue-500 to-purple-500 cursor-pointer rounded-r-md border-l-transparent hover:border-l-black hover:dark:border-l-gray-400 border-l-4 active:text-white duration-200" key={i}>
-                                    <h3>{item.tab}</h3>
-                                </div>
+                                <Link href={item.path} key={i}>
+                                    <div className="font-tisa items-center flex w-full justify-center p-1 hover:bg-gradient-to-r from-blue-500 to-purple-500 cursor-pointer rounded-r-md border-l-transparent hover:border-l-black hover:dark:border-l-gray-400 border-l-4 active:text-white duration-200" key={i}>
+                                        <h3>{item.tab}</h3>
+                                    </div>
+                                </Link>
                             )
                         )}
                     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,20 +4,8 @@ import { useVideoAndDarkMode } from '../utils';
 import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
 import sticky_luffy from "../../public/sticky_luffy.png";
 import dragon from "../../public/dragon.png"
-import Navbar from './navbar';
+import Navbar from '../navbar';
 import { useState, useEffect } from 'react';
-
-{/*import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);*/}
 
 type HomeProps = {
   videoOff: boolean;
@@ -58,59 +46,7 @@ export default function Home(props: HomeProps) {
               <video src='op_bg.mp4' autoPlay loop muted className="w-full h-full object-cover object-center"></video>
             </div>
             )}
-        
-          {/*<div className='text-center p-20'>
-            <h2 className='text-6xl py-2 bg-gradient-to-r from-black via-gray-600 to-black text-transparent bg-clip-text font-tisa_bold font-bold md:text-8xl '>
-            </h2>
-             {/*<p className='text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto  md:text-xl dark:text-gray-400'> 
-              </p>
-          </div>*/}
-
-          {/*<div className='gap-10 lg:flex'>
-            <div className='text-center grid place-items-center shadow-lg p-10 rounded-xl my-10'>
-              <Image src={sticky_luffy} alt={'Design icon'} width={100} height={100} />
-              <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful</h3>
-              <p className='py-2'>
-                Creating.
-              </p>
-              <h4 className='py-4 text-blue-400'>
-                <p className='text-gray-800 py-1'>Example 1</p>
-                <p className='text-gray-800 py-1'>Example 2</p>
-                <p className='text-gray-800 py-1'>Example 3 </p>
-              </h4>
-             </div>
-          </div>
-          {/*<div className='text-5xl flex justify-center gap-20 py-3 text-gray-900 dark:text-gray-400'>
-            <a href="https://github.com/dragmakex" target='_blank'>
-              <AiFillGithub />
-            </a>
-            <a href="https://twitter.com/dragmakex" target='_blank'>
-              <AiFillTwitterCircle />
-            </a>
-            <a href="https://linkedin.com/in/astaikov" target='_blank'>
-              <AiFillLinkedin />
-            </a>
-            </div>*/}
-          {/*<div className={`inset-0 border-8 border-gray-400 dark:border-white flex items-center relative mx-auto rounded-full w-80 h-80 mt-24 overflow-hidden md:h-96 md:w-96 dark:bg-gray-400 bg-black transition-all duration-700 ${loaded ? 'opacity-100 translate-y-10' : 'opacity-0 translate-y-0'}`}>
-            <Image src={sticky_luffy} fill objectFit="cover" alt={'Picture'} className='border-lg border-white'/>
-          </div>*/}
         </section>
-
-        {/*<section>
-          <div>
-            <h3 className='text-3xl py-1'>
-              Stuff
-            </h3>
-            <p className='text-md py-2 leading-8 text-gray-900'>
-              Lorem ipsum <span className='text-blue-600'>yeyeyeye</span> dolores
-            </p>
-            <p className='text-md py-2 leading-8 text-gray-900'>
-              Lorem ipsum <span className='text-blue-600'>yeyeyeye</span> dolores
-            </p>
-          </div>
-
-          
-        </section>*/}
       </main>
     </div>
   )

89550b964312060e5e957ca1114241c24838680f
Sticky <dragmakex@gmail.com>
2024-03-20 15:47:40 +0100

    changed favicon order

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -66,8 +66,8 @@ export default function About(props: HomeProps) {
         <meta name="robots" content="index, follow"></meta>
 
         <link rel="shortcut icon" href="/images/favicon.ico"/>
-        <link rel="icon" href="/images/favicon.ico"/>
         <link rel="icon" href="/images/favicon.png"/>
+        <link rel="icon" href="/images/favicon.ico"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_16x16.png"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />

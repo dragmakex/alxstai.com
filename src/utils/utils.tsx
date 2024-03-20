3c0662006fcfaac0cdb22c514ed53e4b49e18d44
Sticky <dragmakex@gmail.com>
2024-03-20 15:04:47 +0100

    tried cookies

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,25 +1,41 @@
+import { parseCookies, setCookie } from 'nookies';
 import { useState } from 'react';
 
 export const useVideoAndDarkMode = () => {
-  const [videoOff, setVideo] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+
+  const cookies = parseCookies();
+  const initialVideoOff = cookies.videoOff === 'false';
+  const initialDarkMode = cookies.darkMode === 'true'; 
+
+  const [videoOff, setVideo] = useState(initialVideoOff);
+  
+  const [darkMode, setDarkMode] = useState(initialDarkMode);
+
+  //const [videoOff, setVideo] = useState(false);
+  //const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
-    if (videoOff) {
-      setDarkMode(!darkMode);
-    } else {
-      setDarkMode(false);
-    }
+    const newDarkMode = videoOff ? !darkMode : false;
+    setDarkMode(newDarkMode);
+    setCookie(null, 'darkMode', String(newDarkMode), {
+      maxAge: 30 * 24 * 60 * 60,
+      path: '/',
+    });
   }
 
   const toggleVideo = () => {
-    if (videoOff) {
-      setVideo(!videoOff)
-      setDarkMode(false)
-    } else {
-      setVideo(!videoOff)
-      setDarkMode(true)
-    }
+    const newVideoOff = !videoOff;
+    const newDarkMode = newVideoOff ? false : true;
+    setVideo(newVideoOff);
+    setDarkMode(newDarkMode);
+    setCookie(null, 'videoOff', String(newVideoOff), {
+      maxAge: 30 * 24 * 60 * 60,
+      path: '/',
+    });
+    setCookie(null, 'darkMode', String(newDarkMode), {
+      maxAge: 30 * 24 * 60 * 60,
+      path: '/',
+    });
   }
 
   return {

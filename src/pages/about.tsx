47ea118789b5041a2d64511f656db64c9f2cf877
Sticky <dragmakex@gmail.com>
2024-09-11 13:46:16 +0200

    changed py about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -84,7 +84,7 @@ export default function About(props: HomeProps) {
               <video ref={videoRef} src='op_bg.mp4' autoPlay loop muted playsInline className="w-full h-full object-cover object-center"></video>
             </div>
             )}
-            <div className='2xl:py-40 xl:py-20'>
+            <div className='2xl:py-40 xl:py-10'>
               <div className={`inset-0 border-4 border-gray-600 dark:border-white flex items-center relative mx-auto rounded-full w-60 h-60 md:w-96 md:h-96 2xl:h-120 2xl:w-120 mt-10 mb-10 overflow-hidden dark:bg-gray-400 bg-black transition-all duration-700 ${loaded ? 'opacity-100 translate-y-10' : 'opacity-0 translate-y-0'}`}>
                   <Image src={sticky_luffy} fill objectFit="cover" alt={'Picture'} className='border-lg border-white'/>
               </div>

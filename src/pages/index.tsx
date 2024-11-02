a76437421757a76232fbeddbc8546748eed161b5
Sticky <dragmakex@gmail.com>
2024-11-02 18:00:51 +0200

    formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,7 +77,7 @@ export default function Home(props: HomeProps) {
               </div>
             )}
 
-          <div className={`text-5xl md:text-8xl 2xl:text-9xl text-center font-tisa_bold font-bold mt-10 py-40 2xl:py-96 dark:text-gray-400 transition-all duration-700 ${loaded ? 'opacity-100 translate-y-10' : 'opacity-0 translate-y-0'}`}>
+          <div className={`text-5xl md:text-8xl 2xl:text-9xl text-center font-tisa_bold font-bold mt-16 py-40 2xl:py-96 dark:text-gray-400 transition-all duration-700 ${loaded ? 'opacity-100 translate-y-10' : 'opacity-0 translate-y-0'}`}>
             hi, i'm <Link href="/about">
               <span className={`hover:text-transparent bg-clip-text hover:bg-gradient-to-r from-blue-400 via-white to-yellow-300 hover:transition-all hover:duration-500`}>
                 alex
